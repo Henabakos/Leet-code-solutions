adec1d608febf81f3ffb713eb13e8fe7ec9128f1
Henok Assefa <151548202+Henabakos@users.noreply.github.com>
2024-11-11 16:39:57 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (98.00%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
@@ -3,8 +3,9 @@ class Solution:
         if a == 0:
             return b
         return gcd(b%a , a)
-        
+
     def findGCD(self, nums: List[int]) -> int:
-        max_elt = max(nums)
-        min_elt = min(nums)
+        nums.sort()
+        max_elt = nums[-1]
+        min_elt = nums[0]
         return gcd(max_elt , min_elt)
